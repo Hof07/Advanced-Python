47052f588cdf7e1a3d65c0b29bdfd6c311bf678c
khanni1 <khanjanc.pro@gmail.com>
2026-07-12 00:26:52 +0530

    some lambda practice

--- lamda/new.py
+++ lamda/new.py
@@ -1,10 +1,31 @@
-# List answer  will be second highest
 # x = [10,30,32,44]
 # can = lambda a,b : 
-# Check Palindrome from string 
 # Count a number of vowels in strings 
-# Find a factorial of 5 to 10
 # Find the LCM of 2 numbers 
-# Find highest the frequency of a character in a string  
 # Find the student with the highest result.(using dictonary)
 # and practice today's function 
+
+
+# Find highest the frequency of a character in a string  
+text = "khanjan is Great python programmer goooddd"
+
+freq = lambda s: max(set(s)-{" "},key=lambda ch: s.count(ch))
+
+print(freq(text))
+
+# Find a factorial of 5 to 10
+fact = lambda x: x*fact(x-1) if x != 0 else 1
+
+print(fact(6))
+
+# Check Palindrome from string 
+ispalin = lambda s: s == s[::-1]
+
+print(ispalin("level"))
+
+# List answer  will be second highest
+lst1 = [10,20,320,10,430,102,43,500]
+
+second_high = lambda lst: sorted(lst,reverse=True)[1]
+
+print(second_high(lst1))
